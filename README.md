b1bec505c83b24d3d6690471148be4161c314d4f
snowball <snowball@inbox.lv>
2023-03-02 21:25:03 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
-Blank project.
+A library for generating and inspecting `LR` parsing tables.  
+Supports `LR(0)`, `SLR` or `LR(1)`, whichever you prefer.
 
-## Build
+## Usage
+
+There's no `CLI`, sorry!  
+Include [inc/pgen/pgen.h](inc/pgen/pgen.h) and [src/pgen.c](src/pgen.c) into your project.  
+Do check out [this example](./tests/exp.c) of how to define a simple grammar and dump all generated tables and sets.
+
+## DOT
+
+For better readability, state transitions can be dumped in [DOT](https://en.wikipedia.org/wiki/DOT_(graph_description_language)).
+
+```
+S -> exp
+exp -> exp + NUM
+exp -> NUM
+exp -> ( exp )
+```
 
 ```bash
-make
+dot states.dot -T png -o states.png
 ```
+
+![](states.png)
--- tests/exp.c
+++ tests/exp.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <pgen/pgen.h>
 
+// S -> exp
+// exp -> exp + NUM
+// exp -> NUM
+// exp -> ( exp )
+
 struct {
     int S;
     int exp;
@@ -9,11 +14,6 @@ struct {
     int L_PAREN;
     int R_PAREN;
 } S;
-
-// S -> exp
-// exp -> exp + NUM
-// exp -> NUM
-// exp -> ( exp )
 
 int main(int argc, char **argv) {
     Grammar *g = newgrammar();
@@ -26,15 +26,31 @@ int main(int argc, char **argv) {
     S.S = addsym(g, newsym(S_NON_TERM, "S"));
     S.exp = addsym(g, newsym(S_NON_TERM, "exp"));
 
+    // right-hand sides must be termianted by a 0
+    // 0 is the id of a reserved/invalid symbol
+    // for ε-productions your right-hand sides would just contain a 0
+    
+    // S -> exp
     addrule(g, newrule(S.S, (int[]){S.exp, 0}));
+
+    // exp -> exp + NUM
     addrule(g, newrule(S.exp, (int[]){S.exp, S.PLUS, S.NUM, 0}));
+
+    // exp -> NUM
     addrule(g, newrule(S.exp, (int[]){S.NUM, 0}));
+
+    // exp -> ( exp )
     addrule(g, newrule(S.exp, (int[]){S.L_PAREN, S.exp, S.R_PAREN, 0}));
 
+    // select type of table you'd like to generate
     g->type = G_SLR;
+
+    // specify your start symbol
     g->start = S.S;
 
+    // generate parsing table and everything else along the way
     bake(g);
+
     dumpall(g);
     dotdumpstates(g, "states.dot");
 
